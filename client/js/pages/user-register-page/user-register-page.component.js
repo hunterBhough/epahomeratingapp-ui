67159e85e4cbbb7ce6947baef02ac2ea5519a31e
Nik Gunawan <nik.gunawan@icf.com>
2019-08-12 17:07:12 +0000

    Squashed commit of the following
    commit 6b4736df 
    Author: Nik Gunawan <nikkig2223@gmail.com> 
    Date: Tue Jul 30 2019 17:43:28 GMT-0400 (Eastern Daylight Time) 
    
        copy privacy policy over to new user page
    
    
    
    commit 32030ff3 
    Author: Nik Gunawan <nikkig2223@gmail.com> 
    Date: Tue Jul 30 2019 17:42:51 GMT-0400 (Eastern Daylight Time) 
    
        add device supported
    
    
    
    commit 7cd49a1b 
    Author: Nik Gunawan <nikkig2223@gmail.com> 
    Date: Tue Jul 30 2019 17:42:25 GMT-0400 (Eastern Daylight Time) 
    
        make terms of service box bigger

--- client/js/pages/user-register-page/user-register-page.component.js
+++ client/js/pages/user-register-page/user-register-page.component.js
@@ -1,6 +1,8 @@
 import template from './user-register-page.html';
 import controller from './user-register-page.controller';
 
+import './user-register-page.scss';
+
 let userRegisterPageComponent = {
     bindings : {
         user : '<'
